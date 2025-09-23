db2f03b03feca60e2f1f9957c360f5a9fd0d888f
dnguy107 <dnguy107@jh.edu>
2025-09-23 13:35:26 +0700

    feat/module4: implement module 4 solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,32 +33,74 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+  // Helper function to get first letter of name in lowercase
+  function getStringFirstLetterLowercase(str) {
+    return str.charAt(0).toLowerCase();
   }
-}
+
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  // See Lecture 50, part 1
+  console.log('====== For loop implementation: ======');
+  for (let nameString of names) {
+
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    var firstLetter = getStringFirstLetterLowercase(nameString);
+
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(nameString);
+    } else {
+      helloSpeaker.speak(nameString);
+    }
+  }
+
+  // Array.prototype.map implemented with speakSimple functions
+  console.log('====== Map implementation with speakSimple functions: ======');
+  function greetAll (nameItem) {
+    var firstLetter = getStringFirstLetterLowercase(nameItem);
+    if (firstLetter === 'j') {
+      console.log(byeSpeaker.speakSimple(nameItem));
+    } else {
+      console.log(helloSpeaker.speakSimple(nameItem));
+    }
+  }
+  names.map(greetAll)
+
+  // Array.prototype.reduce implemented with speakSimple functions
+  console.log('====== Reduce implementation with speakSimple functions: ======');
+  function reduceGreetAll (accumulator, nameItem) {
+    var firstLetter = getStringFirstLetterLowercase(nameItem);
+    if (firstLetter === 'j') {
+      accumulator.bye.push(byeSpeaker.speakSimple(nameItem));
+    } else {
+      accumulator.hello.push(helloSpeaker.speakSimple(nameItem));
+    }
+    return accumulator;
+  }
+  let reducedGreetAll = names.reduce(reduceGreetAll, {hello: [], bye: []});
+
+  console.log('--> Hello greetings:');
+  for (let helloGreeting of reducedGreetAll.hello) {
+    console.log(helloGreeting);
+  }
+  console.log('--> Bye greetings:');
+  for (let byeGreeting of reducedGreetAll.bye) {
+    console.log(byeGreeting);
+  }
+
+})();
